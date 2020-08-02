cb86f506bc33b2b7eee8bc65d632677e4bfc3428
Jakob Dahl <jakobdahl@gmail.com>
2020-08-02 19:28:39 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import breaks from 'remark-breaks';
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
 import defaultMarkdown from 'DefaultMarkdown';
 import './App.css';
 
@@ -16,37 +16,43 @@ class Editor extends React.Component {
 		this.props.onChange(e.target.value);
 	}
 
-	render() {
-		const value = this.props.value;
-		return (
-			<div className="col-md-10 offset-md-1">
-				<form>
-					<textarea id="editor" value={value} onChange={this.handleChange} />
-				</form>
-			</div>
-		);
-	}
-}
-
-
-const Preview = ({ value }) => {
-	return (
-		<div className="col-md-10 offset-md-1" id="preview">
-			<ReactMarkdown source={value} plugins={[breaks]} />
+	render = () => (
+		<div className="col-md-10 offset-md-1">
+			<form>
+				<textarea
+					id="editor"
+					value={this.props.value}
+					onChange={this.handleChange}
+				/>
+			</form>
 		</div>
-	)
+	);
 }
 
-const Footer = () => {
-	return (
-		<div className="footer">
-			<p>Made with React, much coffee and Stack<strong>overflow</strong></p>
-			<p>freeCodeCamp Markdown Previewer project by Jakob Dahl</p>
-			<p>Code is on <a href="https://github.com/saadanerdetbare/markdownviewer" target="_blank" rel="noopener noreferrer">github</a></p>
-		</div>
-	)
-}
+const Preview = ({ value }) => (
+	<div className="col-md-10 offset-md-1" id="preview">
+		<ReactMarkdown source={value} plugins={[breaks]} />
+	</div>
+);
 
+const Footer = () => (
+	<div className="footer">
+		<p>
+			Made with React, much coffee and Stack<strong>overflow</strong>
+		</p>
+		<p>freeCodeCamp Markdown Previewer project by Jakob Dahl</p>
+		<p>
+			Code is on{' '}
+			<a
+				href="https://github.com/saadanerdetbare/markdownviewer"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				github
+			</a>
+		</p>
+	</div>
+);
 
 class App extends React.Component {
 	constructor(props) {
@@ -60,18 +66,15 @@ class App extends React.Component {
 	handleChange(value) {
 		this.setState({ value: value });
 	}
-	render() {
-		return (
-			<div className="container">
-				<div className="row">
-					<Editor value={this.state.value} onChange={this.handleChange} />
-					<Preview value={this.state.value} />
-					<Footer />
-				</div>
+	render = () => (
+		<div className="container">
+			<div className="row">
+				<Editor value={this.state.value} onChange={this.handleChange} />
+				<Preview value={this.state.value} />
+				<Footer />
 			</div>
-		);
-	}
+		</div>
+	);
 }
 
 export default App;
-
